fc3a89f9baac04e6f6417528f7baa5e7469ccb23
root <nitish9t7>
2019-10-10 13:44:15 +0530

    Initial commit

---
+++ app/src/main/java/com/whysoserious/neeraj/hospitalmanagementsystem/Patient/Wait_Appointment.java
@@ -0,0 +1,27 @@
+package com.whysoserious.neeraj.hospitalmanagementsystem.Patient;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.whysoserious.neeraj.hospitalmanagementsystem.R;
+
+/**
+ * Created by Neeraj on 07-Apr-16.
+ */
+public class Wait_Appointment extends AppCompatActivity {
+
+    String username,password,user_type;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wait_appointment);
+
+        Bundle bb = getIntent().getExtras();
+        username = bb.getString("username");
+        password = bb.getString("password");
+        user_type = bb.getString("user_type");
+    }
+}
---
+++ app/src/main/java/com/whysoserious/neeraj/hospitalmanagementsystem/Services_offered.java
@@ -0,0 +1,23 @@
+package com.whysoserious.neeraj.hospitalmanagementsystem;
+
+import android.database.Cursor;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Neeraj on 14-Apr-16.
+ */
+public class Services_offered extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.services_offered);
+
+    }
+}
